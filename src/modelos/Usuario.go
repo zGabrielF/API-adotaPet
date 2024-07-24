1d547774c7a9bf3cf04c4f8be9b4269eb1c9956c
Gabriel de Freitas <gabrielfsantos.ti@gmail.com>
2024-07-24 00:03:06 -0300

    atualizando os modelos pet e usuario

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -1,4 +1,10 @@
 package modelos
 
 type Usuario struct {
+	ID       uint64 `json:"id,omitempty"`
+	Nome     string `json:"nome,omitempty"`
+	Nick     string `json:"nick,omitempty"`
+	Senha    string `json:"senha,omitempty"`
+	Telefone string `json:"telefone,omitempty"`
+	Pets     []Pet  `json:"pets,omitempty"`
 }
