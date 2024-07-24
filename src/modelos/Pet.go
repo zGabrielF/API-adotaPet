c8532c63e24d0274b3bcb4a11a94e274ca798098
Gabriel de Freitas <gabrielfsantos.ti@gmail.com>
2024-07-24 00:08:43 -0300

    teste

--- src/modelos/Pet.go
+++ src/modelos/Pet.go
@@ -1,9 +1,9 @@
 package modelos
 
 type Pet struct {
-	ID        uint64 `json:"id,omitempty"`
-	Nome      string `json:"nome,omitempty"`
-	Raca      string `json:"raca,omitempty"`
-	UsuarioID uint64 `json:"usuario,omitempty"`
-	//Usuario Usuario `json:"usuario,omitempty"`
+	ID        uint64  `json:"id,omitempty"`
+	Nome      string  `json:"nome,omitempty"`
+	Raca      string  `json:"raca,omitempty"`
+	UsuarioID uint64  `json:"usuario,omitempty"`
+	Usuario   Usuario `json:"usuario,omitempty"`
 }
