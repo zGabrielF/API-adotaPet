a7a1ca96fd4fd04a07a2a3369eab0f6442726f3c
Gabriel de Freitas <gabrielfsantos.ti@gmail.com>
2024-07-25 23:50:01 -0300

    adicionado mais campos em pet e usuario

--- src/modelos/Pet.go
+++ src/modelos/Pet.go
@@ -1,9 +1,13 @@
 package modelos
 
 type Pet struct {
-	ID        uint64 `json:"id,omitempty"`
-	Nome      string `json:"nome,omitempty"`
-	Raca      string `json:"raca,omitempty"`
-	UsuarioID uint64 `json:"usuario,omitempty"`
-	//Usuario Usuario `json:"usuario,omitempty"`
+	ID        uint64  `json:"id,omitempty"`
+	Nome      string  `json:"nome,omitempty"`
+	Raca      string  `json:"raca,omitempty"`
+	Cor       string  `json:"cor,omitempty"`
+	Tamanho   string  `json:"tamanho,omitempty"`
+	Sexo      string  `json:"sexo,omitempty"`
+	Idade     uint8   `json:"idade,omitempty"`
+	Peso      float32 `json:"peso,omitempty"`
+	UsuarioID uint64  `json:"usuarioId,omitempty"`
 }
